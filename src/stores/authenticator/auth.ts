bce8b404d858c940e14bf9caa76050240e035390
knarficious <knarficious@free.fr>
2023-09-26 15:16:59 +0200

    l

--- src/stores/authenticator/auth.ts
+++ src/stores/authenticator/auth.ts
@@ -31,7 +31,7 @@ export const useUserAuthStore = defineStore("userAuth", {
           body: JSON.stringify(payload),
         });
 
-        console.log('response: ', response.headers);
+        console.log('response: ', response);
 
         const {cookies} = useCookies();
         const jwt = cookies.get("jwt_hp");
